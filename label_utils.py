969430175baab95b4c8b836b0404d29f4817f068
jgdolot <104575778+jgdolot@users.noreply.github.com>
2022-05-03 04:03:49 +0800

    Add files via upload

--- label_utils.py
+++ label_utils.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 
 from matplotlib.patches import Rectangle
 from random import randint
+from torchvision import transforms
 
 def get_box_color(index=None):
     """Retrieve plt-compatible color string based on object index"""
@@ -116,6 +117,81 @@ def build_label_dictionary(path):
     classes.insert(0, 0)
     #print("Num of unique classes:: ", classes)
     return dict, classes
+
+def get_label_dictionary_coco(labels, keys):
+    """Associate key (filename) to value (box coords, class)"""
+    dictionary = {}
+    for key in keys:
+        dictionary[key] = [] # empty boxes
+
+    for label in labels:
+        if len(label) != 6:
+            print("Incomplete label:", label[0])
+            continue
+
+        value = label[1:]
+
+        if value[0]==value[1]:
+            continue
+        if value[2]==value[3]:
+            continue
+
+        if label[-1]==0:
+            print("No object labelled as bg:", label[0])
+            continue
+
+        # box coords are float32
+        value = value.astype(np.float32)
+        target = {}
+        bndboxes = [value[0], value[2], value[1], value[3]]
+        target["boxes"] = [bndboxes]
+        target["labels"] = [value[4]]
+        target["image_id"] = int(label[0][:-4])
+        target["area"] = [(value[1]-value[0])*(value[3]-value[2])]
+        target["iscrowd"] = [0]
+        # filename is key
+        key = label[0]
+        # boxes = bounding box coords and class label
+        #boxes = dictionary[key]
+        #boxes.append(value)
+        dictionary[key] = target
+
+    # remove dataset entries w/o labels
+    for key in keys:
+        if len(dictionary[key]) == 0:
+            del dictionary[key]
+
+    return dictionary
+
+def build_label_dictionary_coco(path):
+    """Build a dict with key=filename,
+    value={
+      "boxes":[xmin, ymin, xmax, ymax],
+      "labels":class_id,
+      #"masks":masks,
+      "image_id":image_id
+      "area":(xmax-xmin) * (ymax-ymin)
+      "iscrowd":0
+    }"""
+    labels = load_csv(path)
+    dir_path = os.path.dirname(path)
+    # skip the 1st line header
+    labels = labels[1:]
+    # keys are filenames
+    keys = np.unique(labels[:,0])
+    #keys = [os.path.join(dir_path, key) for key in keys]
+    dict = get_label_dictionary_coco(labels, keys) 
+    #for key in dictionary.keys():
+        #dict[key] = np.array(dictionary[key])
+        #e.g. {0010069.jpg:{Key1:Value1, Key2:Value2, ...}}
+
+    #classes = np.unique(labels[:,-1]).astype(int).tolist()
+    # insert background label 0
+    #classes.insert(0, 0)
+    #print("Num of unique classes:: ", classes)
+    return dict
+
+    
 
 
 def show_labels(image, labels, ax=None):
