aafce7d53e366b934680d13fa0c73b5226757be0
jgdolot <104575778+jgdolot@users.noreply.github.com>
2022-05-29 17:32:56 +0800

    Update kws_infer.py

--- kws_infer.py
+++ kws_infer.py
@@ -45,7 +45,7 @@ def get_args():
     parser.add_argument("--win-length", type=int, default=None)
     parser.add_argument("--hop-length", type=int, default=512)
     parser.add_argument("--wav-file", type=str, default=None)
-    parser.add_argument("--checkpoint", type=str, default="https://github.com/roatienza/Deep-Learning-Experiments/releases/download/models/resnet18-kws-best-acc.pt")
+    parser.add_argument("--checkpoint", type=str, default="data/speech_commands/checkpoints/trans-kws-best-acc.pt")
     parser.add_argument("--gui", default=False, action="store_true")
     parser.add_argument("--rpi", default=False, action="store_true")
     parser.add_argument("--threshold", type=float, default=0.6)
